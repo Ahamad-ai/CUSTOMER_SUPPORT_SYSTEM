7cb4ad43c195829d7000fd8f44de792ca90860ee
Ahamad-ai <ahamad.aihyd@gmail.com>
2025-05-31 18:56:37 +0530

    data updated for reading

--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -2,9 +2,14 @@ import pandas as pd
 from langchain_core.documents import Document
 
 class data_converter:
-    
+
     def __init__(self):
-        pass
+        print("data convert has been init ...")
+        self.product_data=pd.read_csv(r"D:\\Projects\\customer_support_system\\data\\flipkart_product_review.csv")
+        print(self.product_data.head())
 
     def data_transformation(self):
         pass
+
+if __name__ == "__main__":
+    data_transformation=data_converter()
