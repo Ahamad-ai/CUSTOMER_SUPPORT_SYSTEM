a8dbd9f8cd9e070bc70541f6044c3e07afbea975
Ahamad-ai <ahamad.aihyd@gmail.com>
2025-06-01 07:13:39 +0530

    retrieval, css and html added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Request, FOrm
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
