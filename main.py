92cbd002b3728003d7dfe92a4d49daccbfe3f812
Ahamad-ai <ahamad.aihyd@gmail.com>
2025-06-02 08:15:34 +0530

    docker file updated

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
 
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+from retriever.retrieval import Retriever
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import PROMPT_TEMPLATES
+
+
 
 app=FastAPI()
 app.mount("/static", StaticFiles(directory="static"),name="static")
@@ -17,6 +26,24 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+load_dotenv()
+retriever_obj=Retriever()
+model_loader=ModelLoader()
+
+def invoke_chain(query:str):
+    retriever=retriever_obj.load_retriever()
+    prompt=ChatPromptTemplate.from_template(PROMPT_TEMPLATES["product_bot"])
+    llm=model_loader.load_llm()
+
+    chain = (
+        {"context":retriever, "question":RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    result=chain.invoke(query)
+
+    return result
 
 @app.get("/",response_class=HTMLResponse)
 async def index(request: Request):
@@ -24,3 +51,9 @@ async def index(request: Request):
     Render the chat interface
     """
     return templates.TemplateResponse("chat.html", {"request":request})
+
+@app.post("/get",response_class=HTMLResponse)
+async def chat(msg:str=Form(...)):
+    result=invoke_chain(msg)
+    print(f"Response: {result}")
+    return result
